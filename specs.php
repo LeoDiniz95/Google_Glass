17178eff3daa0ddc564dbd39c647b1bc829e0e4f
Leonardo <leonardo.diniz95@yahoo.com.br>
2019-02-26 10:07:11 -0300

    Borda do iframe ainda não sumiu

--- specs.php
+++ specs.php
@@ -1,9 +1,30 @@
+<link rel="stylesheet" href="_css/specs.css" />
 
-Glass > Especificações
-Raio-X no Google Glass
-por Leonardo Diniz
-Atualizado em XX/XXX/XXXX
+<section id="corpo-full">
+    
+    <article id="noticia-principal">
+                
+        <header id="cabecalho-artigo">
 
-Clique em qualquer área destacada da imagem para ter mais informações sobre os recursos do Google Glass. Qualquer ponto vermelho vai te levar a um lugar cheio de novas informações.
+            <hgroup>
+                <h3>Glass > Especificações</h3>
+                <h1>Raio-X no Google Glass</h1>
+                <h2>por Leonardo Diniz</h2>
+                <h3 class="direita">Atualizado em XX/XXX/XXXX</h3>
+             </hgroup>
+                    
+        </header>
 
-[AQUI ENTRA UMA IMAGEM]
+        <p>Clique em qualquer área destacada da imagem para ter mais informações sobre os recursos do Google Glass. Qualquer ponto vermelho vai te levar a um lugar cheio de novas informações.</p>
+
+        <section id="conteudo">
+            
+            <img src="_imagens/glass-esquema-marcado.jpg" />
+            
+            <iframe id="frame-spec" name="janela" src="google-glass.php"></iframe>
+            
+        </section>
+        
+    </article>
+    
+</section>
